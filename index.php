97818a11bed432cdfbdb94d8b3d81356e677ca80
mkvalsvik <mkvalsviK@scandilabs.com>
2012-09-21 11:42:31 -0400

    Added placeholder copy

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    
+</head>
+<body>
+<h1>Catamaran Framework makes building web applications with Java easy.  It seamlessly ties together best-practice Spring MVC, Hibernate, Tomcat, HTML/CSS, and jQuery in a ready-to-use framework, without sacrificing any of the power and flexibility of Java and industry standard Java application tools.</h1>
+
+<h2>Features:</h2>
+<ul>
+    <li>Standard Java throughout, no language enhancements, separate compilers or pre-compilers necessary</li>
+    <li>Leverages true Object Oriented programming with method-rich Domain Model objects that puts behavior where it belongs, not where the framework needs it to be.  Ref: http://martinfowler.com/eaaCatalog/domainModel.html</li>
+    <li>Like the Spring Framework, Catamaran adheres to a Plain Old Java Object (POJO) philosophy that retains all the power and flexibility of Java while freeing developers from having to make lots of minor design decisions to get up and running.</li>
+</ul>
+
+<p>
+    Start with our tutorial:link today, and have a running application up in minutes!
+</p>
+
+</body>
+</html>
