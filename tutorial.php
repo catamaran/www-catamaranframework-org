3ecc2b8f696defa617e526055ac488e34e765bcf
Mads Kvalsvik <mkvalsvik@scandilabs.com>
2012-12-26 16:56:53 -0500

    First few pages of tutorial.  Nice formatting based on django tutorial

--- tutorial.php
+++ tutorial.php
@@ -5,6 +5,7 @@
     <link type="text/css" rel="stylesheet" href="css/site.css" media="screen, projection">
     <link type="text/css" rel="stylesheet" href="css/top.css" media="screen, projection">
     <link type="text/css" rel="stylesheet" href="css/wordpress.css" media="screen, projection">
+    <link type="text/css" rel="stylesheet" href="css/tutorial.css" media="screen, projection">
     
 </head>
 <body>
@@ -14,18 +15,12 @@
             <?php include("include/top.php"); ?>
             <?php include("include/nav.php"); ?>
             
-            <h1>Catamaran Tutorial</h1>
             <p>
                 <?php
                     $tutorial = file_get_contents('tutorial.md');                    
                     echo Markdown($tutorial); 
                 ?>
 
-
-                Still working on this, check back soon!<br/><br/><br/>
-                
-                <img src="img/Under-Construction.gif"/>
-                
             </p>
             
             <!-- footer -->
